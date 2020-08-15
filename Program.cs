f4dcbcfa62d1e83a5314ba660935dc0da55fa67c
Omkar <omkar.prabesh@gmail.com>
2020-08-15 10:24:58 -0400

    first commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,54 @@
 
 namespace EventSample
 {
-    class Program
+    public class EventDemo
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Listener1 l1 = new Listener1();
+
+
+            EventClass a1 = new EventClass();
+            a1.eventFired("Here we go!");
+
+
+            a1.DemoEvent += l1.listener1Message;
+           
+
+        }
+        public class BName : EventArgs
+        {
+            public string EventInfo { get; set; }
+            public BName(string eInfo)
+            {
+                EventInfo = eInfo;
+            }
+        }
+
+        public delegate void SomeEventHandler(object source, BName b);
+
+
+
+        public class EventClass
+        {
+
+            public event SomeEventHandler DemoEvent;
+
+            public void eventFired(string x)
+            {
+
+                if (DemoEvent != null)
+                {
+                    DemoEvent(this, new BName(x));
+                }
+            }
+        }
+        public class Listener1
+        {
+            public void listener1Message(object sender, BName b)
+            {
+                Console.WriteLine($"Event was fired with {b.EventInfo}");
+            }
         }
     }
 }
